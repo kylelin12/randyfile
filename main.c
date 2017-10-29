10e415935c130800c90697f22662bf7dee8e0f58
Kyle Lin <klin12@stuy.edu>
2017-10-29 10:44:39 -0400

    Finished

--- main.c
+++ main.c
@@ -9,15 +9,45 @@
 
 int main() {
     int fd;
-    srand(time(NULL));
+    int random;
+    int numran[10];
 
-    fd = open('/dev/random', O_RDONLY);
-    read()
-    int rnumandom[10];
+    // Generating random numbers and populating the array
+    printf("Generating random numbers: \n");
 
     int i;
     for (i = 0; i < 10; i++) {
-        rnumandom[i] = rand
+        fd = open("/dev/random", O_RDONLY);
+        // If fd < 0 then the user doesn't have /dev/random
+        if (fd < 0) {
+            printf("You appear to be missing a /dev/random directory.\n");
+            break;
+        } else {
+            read(fd, &random, sizeof(random));
+            close(fd);
+            numran[i] = random;
+            printf("\trandom %d: %d\n", i, numran[i]);
+        }
+    }
+
+    // File Writing
+
+    printf("\nWriting numbers to file...\n\n");
+    int fileNums = open("rannums.txt", O_CREAT | O_RDWR, 0644);
+    write(fileNums, numran, sizeof(numran));
+    close(fileNums);
+
+    // File Reading
+    printf("Reading numbers from file...\n\n");
+    int results[10];
+    fileNums = open("rannums.txt", O_RDONLY);
+    read(fileNums, results, sizeof(results));
+    close(fileNums);
+    
+    // Print result
+    printf("Verification that written values were the same:\n");
+    for (i = 0; i < 10; i++) {
+        printf("\trandom %d: %d\n", i, results[i]);
     }
 
     return 0;
